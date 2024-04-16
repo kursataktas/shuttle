ffd8ce1ed799009363228b6ba5e00fb41033c2bf
Iulian Barbu <iulianbarbu2@gmail.com>
2024-04-16 18:19:28 +0300

    cargo-shuttle: add pending ecs state

--- common/src/models/deployment.rs
+++ common/src/models/deployment.rs
@@ -131,6 +131,7 @@ impl EcsState {
     /// we may end up with two different versions of Color.
     pub fn get_color(&self) -> &str {
         match self {
+            EcsState::Pending => "dark_yellow",
             EcsState::InProgress => "cyan",
             EcsState::Running => "green",
             EcsState::Stopped => "dark_blue",
